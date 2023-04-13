99c932b8657c3c79ba54e841a2f75952f2db3c9f
laurentbul <bulent.email@hotmail.fr>
2023-04-13 16:27:56 +0200

    application essaie

---
+++ crud2/student.sql
@@ -0,0 +1,10 @@
+CREATE TABLE students (
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(191) NOT NULL,
+    email VARCHAR(191) NOT NULL,
+    phone VARCHAR(191) NOT NULL,
+    course VARCHAR(191) NOT NULL
+)
+
+INSERT INTO students (id, name, email, , phone , cours)
+VALUES ('1', 'bulent', 'bul@bul.fr', '123456', 'math');
